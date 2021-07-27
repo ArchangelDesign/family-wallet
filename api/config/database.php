784ecd3a385118c6053c44c091fe2d577d506f04
Raff <archangel.raffael@gmail.com>
2021-07-27 10:10:48 -0400

    fixed unit tests to pass in the pipeline

--- api/config/database.php
+++ api/config/database.php
@@ -42,6 +42,13 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        'pdo_sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
 
         'mysql' => [
             'driver' => 'mysql',

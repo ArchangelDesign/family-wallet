5548340615d13541df4c115858debf732f76eec5
Raff-WIndows <archangel.raffael@gmail.com>
2021-07-27 17:07:01 -0400

    PHPCS | TransactionService.php fixed and all services included in the inspection

--- api/app/Services/TransactionService.php
+++ api/app/Services/TransactionService.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Domain service. Central point where all financial operations are performed
+ * php version 7.4
+ *
+ * @category Service
+ * @package  App\Services
+ * @author   Raff <email@domain.com>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @version  GIT: @1.0.0@
+ * @link     https://github.com/ArchangelDesign/family-wallet
+ */
+
 namespace App\Services;
 use App\Entities\Account;
 use App\Entities\Transaction;
@@ -19,36 +31,56 @@ use Illuminate\Database\MultipleRecordsFoundException;
 /**
  * Class TransactionService
  *
- * @package App\Services
+ * @category Service
+ * @package  App\Services
+ * @author   Raff <email@domain.com>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     https://github.com/ArchangelDesign/family-wallet
  */
 class TransactionService
 {
     const SUPPORTED_FORMATS = ['qfx', 'csv'];
     /**
+     * Gate to persistence
+     *
      * @var DatabaseService
      */
-    private $db;
+    private $_db;
 
+    /**
+     * TransactionService constructor.
+     *
+     * @param DatabaseService $db database service
+     */
     public function __construct(DatabaseService $db)
     {
-        $this->db = $db;
+        $this->_db = $db;
     }
 
+    /**
+     * Returns an array of strings representing compatible file formats
+     *
+     * @return string[]
+     */
     public function getSupportedFormats(): array
     {
         return self::SUPPORTED_FORMATS;
     }
 
     /**
-     * @param  string $format
+     * Returns an instance of the parser for given file format
+     *
+     * @param string $format format
+     *
      * @return TransactionFileParserInterface
+     *
      * @throws UnhandledTransactionFileFormat
      */
     public function getParser(string $format): TransactionFileParserInterface
     {
         switch ($format)
         {
-        case 'qfx': 
+        case 'qfx':
             return new QfxTransactionFileParser();
         default:
             throw new UnhandledTransactionFileFormat($format);
@@ -56,48 +88,75 @@ class TransactionService
     }
 
     /**
-     * @param  Account     $account
-     * @param  Transaction $transaction
-     * @param  bool        $flush
+     * Stores transaction in the persistence. Transactions are validated.
+     *
+     * @param Account     $account     synchronized Account entity
+     * @param Transaction $transaction raw (detached) Transaction entity.
+     * @param bool        $flush       commit transaction
+     *
+     * @return void
+     *
      * @throws AccountNotFound
      * @throws TransactionDuplicated
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
      */
-    public function registerTransaction(Account $account, Transaction $transaction, $flush = true)
-    {
+    public function registerTransaction(
+        Account $account,
+        Transaction $transaction,
+        $flush = true
+    ) {
         if (empty($account->getId())) {
             throw new AccountNotFound();
         }
-        if (!$this->db->contains($account)) {
-            throw new AccountNotFound('Entity is not synchronized with Entity Manager');
+        if (!$this->_db->contains($account)) {
+            throw new AccountNotFound(
+                'Entity is not synchronized with Entity Manager'
+            );
         }
         try {
-            $this->fetchTransactionByValues($transaction);
+            $this->_fetchTransactionByValues($transaction);
             throw new TransactionDuplicated($transaction->getTransactionId());
         } catch (TransactionNotFound | TransactionDuplicateFound $e) {
         }
         $transaction->setAccount($account);
-        $this->db->persist($transaction, $flush);
-    }
-
-    public function fetchAccountByIdOrName($accountIdOrName): Account
-    {
-        if (is_numeric($accountIdOrName)) {
-            return $this->fetchAccountById($accountIdOrName);
-        }
-
-        return $this->fetchAccountByName($accountIdOrName);
+        $this->_db->persist($transaction, $flush);
     }
 
     /**
-     * @param  int $accountId
+     * Returns Account entity by given name or ID (primary key from `accounts` table)
+     *
+     * @param int|string $accountIdOrName either ID or name
+     *
      * @return Account
+     *
+     * @throws AccountNotFound
+     * @throws MultipleAccountsMatched
+     * @throws TransactionServicePanic
+     */
+    public function fetchAccountByIdOrName($accountIdOrName): Account
+    {
+        if (is_numeric($accountIdOrName)) {
+            return $this->_fetchAccountById($accountIdOrName);
+        }
+
+        return $this->_fetchAccountByName($accountIdOrName);
+    }
+
+    /**
+     * Returns Account entity by primary key
+     *
+     * @param int $accountId primary key from `transactions` table
+     *
+     * @return Account
+     *
      * @throws AccountNotFound
      * @throws TransactionServicePanic
      */
-    private function fetchAccountById(int $accountId): Account
+    private function _fetchAccountById(int $accountId): Account
     {
         try {
-            return $this->db->getEntityManager()->createQueryBuilder()
+            return $this->_db->getEntityManager()->createQueryBuilder()
                 ->select('a')
                 ->from('App\Entities\Account', 'a')
                 ->where('a.id = :id')
@@ -107,20 +166,26 @@ class TransactionService
         } catch (NoResultException $e) {
             throw new AccountNotFound('id: ' . $accountId);
         } catch (NonUniqueResultException $e) {
-            throw new TransactionServicePanic('More than one account found for id ' . $accountId);
+            throw new TransactionServicePanic(
+                'More than one account found for id ' . $accountId
+            );
         }
     }
 
     /**
-     * @param  string $accountName
+     * Returns Account entity by given name
+     *
+     * @param string $accountName exact account name
+     *
      * @return Account
+     *
      * @throws AccountNotFound
      * @throws MultipleAccountsMatched
      */
-    private function fetchAccountByName(string $accountName): Account
+    private function _fetchAccountByName(string $accountName): Account
     {
         try {
-            return $this->db->getEntityManager()->createQueryBuilder()
+            return $this->_db->getEntityManager()->createQueryBuilder()
                 ->select('a')
                 ->from('App\Entities\Account', 'a')
                 ->where('a.name = :name')
@@ -129,26 +194,43 @@ class TransactionService
                 ->getSingleResult();
         } catch (NoResultException $e) {
             throw new AccountNotFound($accountName);
-        } catch (NonUniqueFieldNameException $e) {
-            throw new MultipleAccountsMatched('More than one account matches ' . $accountName);
+        } catch (NonUniqueResultException $e) {
+            throw new MultipleAccountsMatched(
+                'More than one account matches ' . $accountName
+            );
         }
     }
 
-    public function flush()
+    /**
+     * Commits uncommitted transactions
+     *
+     * @return void
+     *
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function flush(): void
     {
-        $this->db->flush();
+        $this->_db->flush();
     }
 
     /**
-     * @param  Transaction $transaction
+     * Returns transaction that supposedly is the same one as given, a duplicate.
+     * To achieve that, this method will try to match values of the transaction
+     * to any existing one however this violates the assumption that we're getting
+     * a valid file from the bank. In testing it became known that some transactions
+     * are marked as duplicates which should not be the case.
+     *
+     * @param Transaction $transaction either attached or detached Transaction entity
+     *
      * @return Transaction
      * @throws TransactionNotFound
      * @throws TransactionDuplicateFound
      */
-    private function fetchTransactionByValues(Transaction $transaction): Transaction
+    private function _fetchTransactionByValues(Transaction $transaction): Transaction
     {
         try {
-            return $this->db->getEntityManager()->createQueryBuilder()
+            return $this->_db->getEntityManager()->createQueryBuilder()
                 ->select('t')
                 ->from('App\Entities\Transaction', 't')
                 ->where('t.transactionId = :id')
@@ -173,34 +255,42 @@ class TransactionService
     /**
      * For unit testing purposes only
      *
-     * @internal
-     * @param    Transaction $transaction
-     * @throws   \Doctrine\ORM\ORMException
+     * @param Transaction $transaction attached entity
+     *
+     * @return void
+     *
+     * @throws \Doctrine\ORM\ORMException
      */
     public function deleteTransaction(Transaction $transaction)
     {
-        $this->db->getEntityManager()->remove($transaction);
-        $this->db->flush();
+        $this->_db->getEntityManager()->remove($transaction);
+        $this->_db->flush();
     }
 
     /**
-     * @param  string   $name
-     * @param  int|null $id
+     * Creates an account
+     *
+     * @param string   $name account name
+     * @param int|null $id   optional internal account ID
+     *
      * @return Account
+     *
      * @throws AccountAlreadyExists
      * @throws TransactionServicePanic
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
      */
     public function registerAccount(string $name, int $id = null)
     {
         try {
-            $this->fetchAccountByName($name);
+            $this->_fetchAccountByName($name);
             throw new AccountAlreadyExists($name);
         } catch (AccountNotFound $e) {
         } catch (MultipleAccountsMatched $e) {
         }
         try {
             if (!is_null($id)) {
-                $this->fetchAccountById($id);
+                $this->_fetchAccountById($id);
                 throw new AccountAlreadyExists('id: ' . $id);
             }
         } catch (AccountNotFound $e) {
@@ -208,23 +298,31 @@ class TransactionService
         $acc = new Account();
         $acc->setId($id)
             ->setName($name);
-        $this->db->persist($acc, true);
+        $this->_db->persist($acc, true);
 
         return $acc;
     }
 
     /**
-     * @param    Account $acc
-     * @throws   AccountNotFound
+     * Deletes account from persistence layer. For internal use only.
+     *
+     * @param Account $acc synchronized entity
+     *
+     * @return void
+     *
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws AccountNotFound
+     *
      * @internal
      */
     public function deleteAccount(Account $acc)
     {
-        if (!$this->db->contains($acc)) {
+        if (!$this->_db->contains($acc)) {
             throw new AccountNotFound('Entity is not synchronized.');
         }
 
-        $this->db->remove($acc);
-        $this->db->flush();
+        $this->_db->remove($acc);
+        $this->_db->flush();
     }
 }
